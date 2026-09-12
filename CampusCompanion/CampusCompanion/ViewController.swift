2b93d10db00f41c0c67ff9a6679a869f14858ae8
Aleson Depano <alesondepano@gmail.com>
2026-09-12 16:43:49 +0800

    Add files via upload

---
+++ CampusCompanion/CampusCompanion/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  CampusCompanion
+//
+//  Created by DE PANO, ALESON AXEL on 9/12/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        titleLabel.text = "Campus Companion"
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
